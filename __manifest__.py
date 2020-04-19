d49f7f00d3ed8ad905cb8b1803cca3f34adfec7a
Manuel Gomez <manuel@vauxoo.com>
2020-04-18 22:43:21 -0500

    [ADD] Creating a basic module

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Academy B&F",
+    'category': 'Website',
+    'description': "Generic modulo for Bend And Frank",
+    'version': '11.0',
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'board'],
+    # actulizar lista de aplicaciones desde la app. modeo debug
+    # always loaded Importa el orden
+    'data': [
+    ],
+    'instalable': True,
+    'auto_install': True,
+}
