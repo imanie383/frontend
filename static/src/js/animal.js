f6f67ec5851350cf07317363ec592a6c5bbd0ac8
Manuel Gomez <manuel@vauxoo.com>
2020-04-23 13:42:28 -0500

    [ADD] New Class

--- static/src/js/animal.js
+++ static/src/js/animal.js
@@ -8,4 +8,31 @@ odoo.define('academy.animal', function (require){
 	// Comprobar que se cargo la libreria
 	console.log('Libreria cargada')
 
-});
+	//la clase base de odoo se encuentra en web
+	var Class = require('web.Class');
+
+	var Animal = Class.extend({
+		/* Inicializa la clase */
+		init: function () {
+			this.x = 0;
+			this.hunger = 0; 
+
+		},
+		move: function () {
+			this.x = this.x + 1;
+			this.hunger = this.hunger + 1;
+			console.log(this);
+		},
+		eat: function () {
+			this.hunger = 0;
+			console.log(this);
+		}
+
+	});
+
+	var animal = new Animal();
+	animal.move();
+	animal.move();
+	animal.eat();
+
+}); 
